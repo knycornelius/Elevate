0eed33173e6eaa5bf8ebefb1b7f76df0abf023aa
alvin2723 <55695349+alvin2723@users.noreply.github.com>
2019-12-08 12:32:31 +0700

    Update forgot-password.php

--- application/views/pages/forgot-password.php
+++ application/views/pages/forgot-password.php
@@ -8,7 +8,7 @@
         <div class="container">
             <ol class="breadcrumb breadcrumb1 animated wow slideInLeft animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: slideInLeft;">
                 <li><a href="<?php echo base_url(''); ?>"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
-                <li class="active">Account</li>
+                <li class="active">Forgot Your Password</li>
             </ol>
         </div>
     </div>
